b84d36d0a137545b64a3a1944a9c154c552fad2c
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2023-10-31 10:57:24 +0500

    completed react template

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,7 +1,11 @@
-export const HeroSection = ()=>{
-    return (
-        <>
-            <h1>Hero Section</h1>
-        </>
-    )
-}
+// import Container from 'react-bootstrap/Container';
+import Badge from "react-bootstrap/Badge";
+import { Button } from "react-bootstrap";
+
+export const HeroSection = () => {
+  return (
+    <div>
+      <h1>Hero Section</h1>
+    </div>
+  );
+};
